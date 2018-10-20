32972932108883bf7f3a9edec07519a8d965cf8c
Minho Kim <minho.kim093@gmail.com>
2018-10-20 22:11:18 +0900

    Update TLE.java

--- H/solution/test/TLE.java
+++ H/solution/test/TLE.java
@@ -1,15 +1,15 @@
 import java.util.*;
 import java.io.*;
 
-public class tlSolution {
+public class Solution {
 	public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
+		st = new StringTokenizer(br.readLine());
 		int[] queue = new int[N + 1];
 		for (int i = 1; i <= N; i++)
 			queue[i] = Integer.parseInt(st.nextToken());
@@ -19,16 +19,16 @@ public class tlSolution {
 			psum[i] = psum[i - 1] + queue[i];
 
 		while (M-- > 0) {
-        	int T = Integer.parseInt(br.readLine());
+			int T = Integer.parseInt(br.readLine());
 
-            int i = 1;
-            for (; i <= N; i++)
-                if (psum[i] > T)
-                    break;
+			int i = 1;
+			for (; i <= N; i++)
+				if (psum[i] > T)
+					break;
 
-            bw.write((i - 1) + "\n");
+			bw.write((i - 1) + "\n");
 		}
 
-        bw.close();
+		bw.close();
 	}
 }
