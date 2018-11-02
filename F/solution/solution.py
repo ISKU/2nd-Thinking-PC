297cc13218cd288af4e4e89d862939e71f7ff0f0
Minho Kim <minho.kim093@gmail.com>
2018-11-02 15:07:22 +0900

    Add solution code written in Python for F

---
+++ F/solution/solution.py
@@ -0,0 +1,16 @@
+R, C, Q = map(int, input().split())
+
+psum = [[0 for cols in range(C + 1)] for rows in range(R + 1)]
+
+for r in range(1, R + 1):
+    cols = list(map(int, input().split()))
+    for c in range(1, C + 1):
+        psum[r][c] = cols[c - 1] + psum[r - 1][c] + psum[r][c - 1] - psum[r - 1][c - 1]
+
+for q in range(Q):
+    r1, r2, c1, c2 = map(int, input().split())
+    
+    sum = psum[r2][c2] - psum[r1 - 1][c2] - psum[r2][c1 - 1] + psum[r1 - 1][c1 - 1]
+    n = (r2 - r1 + 1) * (c2 - c1 + 1)
+
+    print(sum // n)
